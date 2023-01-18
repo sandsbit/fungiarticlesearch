d9c24919a739d03bb29bce3ff994c8b231b6ca4f
Nikita Serba <nikitaserba@icloud.com>
2023-01-19 00:26:47 +0200

    Clean main file

--- fas/fas.py
+++ fas/fas.py
@@ -1,16 +1,20 @@
-# This is a sample Python script.
+# This file is part of fungiarticlesearch.
+#
+# fungiarticlesearch is free software: you can redistribute it and/or modify it under the terms of the GNU General
+# Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any
+# later version.
+#
+# fungiarticlesearch is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the
+# implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License along with fungiarticlesearch.
+# If not, see <https://www.gnu.org/licenses/>.
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+
+def main() -> None:
+    pass
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    main()
